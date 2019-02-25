5c4a467a8a056ff25e3fa11f6bf4086289fda62a
fabianboesiger <boesiger.fabian@outlook.com>
2019-02-25 21:55:42 +0100

    Changes

--- Database/src/database/templates/ArrayTemplate.java
+++ Database/src/database/templates/ArrayTemplate.java
@@ -1,5 +1,39 @@
 package database.templates;
 
-public class ArrayTemplate {
+public class ArrayTemplate extends PrimitiveTemplate {
+	
+	public ArrayTemplate(String name) {
+		super(name);
+	}
+
+	@Override
+	public String render() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void parse(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void set(Object object) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object get() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean validate(Errors errors) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
